a4a5dc664bae493ecaf82f7caeb4f6c8795de58a
Jack HK <jackhughesking@gmail.com>
2023-01-30 01:45:22 +0000

    added scaling for window

--- Assets/DeveloperConsole/Scripts/ConsoleUIHandler.cs
+++ Assets/DeveloperConsole/Scripts/ConsoleUIHandler.cs
@@ -10,14 +10,28 @@ namespace  R.DeveloperConsole
 {
     public class ConsoleUIHandler : MonoBehaviour, IDragHandler
     {
+        [SerializeField] private float _UIscale = 1;
+        [SerializeField] private float _UIscaleMin = 0.6f;
+        [SerializeField] private float _UIscaleMax = 1.4f;
+
+        [Header("References")]
         [SerializeField] private Canvas _canvas;
         [SerializeField] private RectTransform _rectTransform;
-
+        
         public void OnDrag(PointerEventData eventData)
         {
             _rectTransform.anchoredPosition += eventData.delta / _canvas.scaleFactor;
         }
 
+        public void SetScale(float scale)
+        {
+            _rectTransform.localScale = new Vector3(scale, scale, scale);
+        }
+
+        public void AddScale(float scale)
+        {
+            _rectTransform.localScale = _rectTransform.localScale + new Vector3(scale, scale, scale);
+        }
     }
 }
 
