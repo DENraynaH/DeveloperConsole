8a9f6f208b594a6c66d9604bcd3f86af0bccdf5d
DENraynaH <103390897+DENraynaH@users.noreply.github.com>
2023-01-30 19:32:31 +0000

    sizing caps

--- Assets/DeveloperConsole/Scripts/ConsoleUIHandler.cs
+++ Assets/DeveloperConsole/Scripts/ConsoleUIHandler.cs
@@ -11,17 +11,20 @@ namespace  R.DeveloperConsole
 {
     public class ConsoleUIHandler : MonoBehaviour, IDragHandler
     {
-        [SerializeField] private float _currentScale = 1;
         [SerializeField] private float _scaleMinimum = 0.6f;
         [SerializeField] private float _scaleMaximum = 1.4f;
         [SerializeField] private TextMeshProUGUI _consoleOutput;
+
+        private Vector3 _scaleMinimumVector => new Vector3(_scaleMinimum, _scaleMinimum, _scaleMinimum);
+        private Vector3 _scaleMaxmiumVector => new Vector3(_scaleMaximum, _scaleMaximum, _scaleMaximum);
+        private float _currentScale => _rectTransform.localScale.x;
 
         public TextMeshProUGUI ConsoleOutput => _consoleOutput;
 
         [Header("References")]
         [SerializeField] private Canvas _canvas;
         [SerializeField] private RectTransform _rectTransform;
-        
+
         public void OnDrag(PointerEventData eventData)
         {
             _rectTransform.anchoredPosition += eventData.delta / _canvas.scaleFactor;
@@ -34,8 +37,17 @@ namespace  R.DeveloperConsole
 
         public void AddScale(float scale)
         {
-            _rectTransform.localScale = _rectTransform.localScale + new Vector3(scale, scale, scale);
+            if (_currentScale + scale > _scaleMaximum) { return; }
+            SetScale(_rectTransform.localScale.x + scale);
         }
+
+        public void SubtractScale(float scale)
+        {
+            if (_currentScale + scale < _scaleMinimum) { return; }
+            SetScale(_rectTransform.localScale.x - scale);
+        }
+        
+        
     }
 }
 
